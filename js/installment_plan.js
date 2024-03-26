77033fe13c41d1ee6dcc84743ff37123db5ddadf
Parnakmuradkhanyan <parnak725@gmail.com>
2024-03-26 19:32:39 +0200

    done some installment stuff

---
+++ js/installment_plan.js
@@ -0,0 +1,22 @@
+document.getElementById('plusButton').addEventListener('click', function() {
+    let months = parseInt(document.getElementById('installmentMonths').innerText);
+    const sum = parseFloat(document.getElementById('installmentSumInput').value);
+    const availableMonths = [3, 6, 9, 12, 24, 48];
+    let nextIndex = availableMonths.indexOf(months) + 1;
+    if (nextIndex >= availableMonths.length) nextIndex = availableMonths.length - 1;
+    months = availableMonths[nextIndex];
+    document.getElementById('installmentMonths').innerText = months;
+    document.getElementById('monthly-payment').innerText = (sum / months).toFixed(2);
+  });
+  
+  document.getElementById('minusButton').addEventListener('click', function() {
+    let months = parseInt(document.getElementById('installmentMonths').innerText);
+    const sum = parseFloat(document.getElementById('installmentSumInput').value);
+    const availableMonths = [3, 6, 9, 12, 24, 48];
+    let prevIndex = availableMonths.indexOf(months) - 1;
+    if (prevIndex < 0) prevIndex = 0;
+    months = availableMonths[prevIndex];
+    document.getElementById('installmentMonths').innerText = months;
+    document.getElementById('monthly-payment').innerText = (sum / months).toFixed(2);
+  });
+  
